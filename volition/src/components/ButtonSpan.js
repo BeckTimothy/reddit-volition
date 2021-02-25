38b1683e721a2c647b64853cd670c2a99bd21137
Timothy Beck <johndaveleet@gmail.com>
2021-02-25 09:09:11 -0700

    made all react testing consolelogs into comments

--- volition/src/components/ButtonSpan.js
+++ volition/src/components/ButtonSpan.js
@@ -10,48 +10,40 @@ export const ButtonSpan = (props) => {
 
 	const [renderStatus, setRenderStatus] = useState(2);
 	const handleRender = () => {
-		console.log(`attempting to force page render... state currently: ${renderStatus}, providing state: ${status}`);
+		//console.log(`attempting to force page render... state currently: ${renderStatus}, providing state: ${status}`);
 		setRenderStatus(status);
 	};
 	const initButtons = (feature) => {
-		console.log(`initButtons(${feature}) is now running...`)
+		//console.log(`initButtons(${feature}) is now running...`)
 		chrome.storage.sync.get(feature, (result) => {
 			status = result[feature];
 			handleRender();
 		})
-		//console.log('outside sync: ')
-		//console.log(status);
-		//status === 1 ? handleRender() : handleRender();
 	}
 	if(renderStatus === 2){initButtons(category)}
 
-
-
 	const handleButtons = () => {
-		console.log('\n\nbutton pressed...');
+		//console.log('\n\nbutton pressed...');
 		freeYourself(category);
 	};
 
 	const freeYourself = (feature) => {
-		console.log(`freeYourself(${feature}) is now running...`)
-
+		//console.log(`freeYourself(${feature}) is now running...`)
 		chrome.storage.sync.get(feature, (result) => {
-			console.log(feature + ' Value currently is ' + result[feature]);
+			//console.log(feature + ' Value currently is ' + result[feature]);
 			status = result[feature];
 			let obj = {};
 			status === 1 ? status = 0 : status = 1;
 			obj[feature] = status;
-			let logText = null
-			status === 1 ? logText = 'enabled' : logText = 'disabled';
+			//let logText = null
+			//status === 1 ? logText = 'enabled' : logText = 'disabled';
 			chrome.storage.sync.set(obj, () => {
-				console.log(`"${feature}" in Chrome storage is now set to ${status}, ${feature.substring(8)} ${logText}.`);
+				//console.log(`"${feature}" in Chrome storage is now set to ${status}, ${feature.substring(8)} ${logText}.`);
 			});
 			handleRender();
 		})
 	}
-
-	console.log(`Rendering button with u status: ${renderStatus}`);
-
+	//console.log(`Rendering button with u status: ${renderStatus}`);
 	if(renderStatus === 0) {
 		return (
 			<Row className={'d-flex justify-content-center'}>
